69d41e782c4656a5dc3c42a76d82ba887f2a2a38
Jonas <jonaslim@noxpwr.com>
2026-06-11 04:35:18 +0800

    minor fix

--- components/shell/Sidebar.tsx
+++ components/shell/Sidebar.tsx
@@ -71,16 +71,6 @@ const NAV: NavItem[] = [
     ),
   },
   {
-    href: "/tasks",
-    label: "Tasks",
-    icon: icon(
-      <>
-        <path d="M9 11l3 3L22 4" />
-        <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
-      </>
-    ),
-  },
-  {
     href: "/settings",
     label: "Settings",
     icon: icon(
